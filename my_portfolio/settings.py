46059274ccdf952ceaa74213ee36618ea008e699
s-y1127 <yooopopo5@gmail.com>
2021-03-16 19:15:36 +0900

    c

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
